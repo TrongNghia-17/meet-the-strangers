09e648297dea3ab005a0a8effbda9731018f845c
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-10 14:36:44 +0700

    video 17

--- app.js
+++ app.js
@@ -13,9 +13,22 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
+let connectedPeers = [];
+
 io.on("connection", (socket) => {
-    console.log("user connected to socket.IO server");
-    console.log(socket.id);
+    connectedPeers.push(socket.id);
+    console.log(connectedPeers);
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+
+        const newConnectedPeers = connectedPeers.filter((peerSocketId) => {
+            peerSocketId !== socket.id;
+        });
+
+        connectedPeers = newConnectedPeers;
+        console.log(connectedPeers);
+    });
 });
 
 server.listen(POST, () => {
