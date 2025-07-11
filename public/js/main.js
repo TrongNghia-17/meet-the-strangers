0f56bd07ad9e668b9f39a688949ebb94fde29b27
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-11 19:48:21 +0700

    video 32

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 import * as store from "./store.js";
 import * as wss from "./wss.js";
 
+// initialization of socketIO connection
 const socket = io("/");
-
 wss.registerSocketEnvents(socket);
+
+// register event for personal code copy button
+const personalCodeCopyButton = document.getElementById("personal_code_copy_button");
+personalCodeCopyButton.addEventListener("click", () => {
+    const personalCode = store.getState().socketId;
+    navigator.clipboard && navigator.clipboard.writeText(personalCode);
+});
