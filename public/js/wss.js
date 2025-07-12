97e34e174268eb6a692b289a3b4e15f30d55718d
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-07-12 09:38:48 +0700

    video 35

--- public/js/wss.js
+++ public/js/wss.js
@@ -1,18 +1,25 @@
 import * as store from "./store.js";
 import * as ui from "./ui.js";
+import * as webRTCHandler from "./webRTCHandler.js";
 
 let socketIO = null;
 
 export const registerSocketEnvents = (socket) => {
-    socket.on("connect", () => {
-        socketIO = socket;
+    socketIO = socket;
 
+    socket.on("connect", () => {
         console.log("successfully connected to socket.io server");
         store.setSocketId(socket.id);
         ui.updatePersonalCode(socket.id);
     });
+
+    socket.on("pre-offer", (data) => {
+        console.log("pre offer came");
+        webRTCHandler.handlePreOffer(data);
+    });
 }
 
 export const sendPreOffer = (data) => {
+    console.log("emmiting to server pre offer event");
     socketIO.emit("pre-offer", data);
 };
