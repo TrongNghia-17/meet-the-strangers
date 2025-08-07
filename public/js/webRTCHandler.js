04a631dae027c02e50bcf70459202ee9923bcca3
NghiaHT <huynhtrongnghia15n@gmail.com>
2025-08-07 13:46:00 +0700

    video 48

--- public/js/webRTCHandler.js
+++ public/js/webRTCHandler.js
@@ -10,6 +10,14 @@ const defaultConstraints = {
     video: true,
 };
 
+const configuration = {
+    iceServers: [
+        {
+            urls: "stun:stun.l.google.com:13902",
+        },
+    ],
+};
+
 export const getLocalPreview = () => {
     navigator.mediaDevices
         .getUserMedia(defaultConstraints)
@@ -21,6 +29,65 @@ export const getLocalPreview = () => {
             console.log("error occured when trying to get an access to camera");
             console.log(err);
         });
+};
+
+const createPeerConnection = () => {
+    peerConection = new RTCPeerConnection(configuration);
+
+    dataChannel = peerConection.createDataChannel("chat");
+
+    peerConection.ondatachannel = (event) => {
+        const dataChannel = event.channel;
+
+        dataChannel.onopen = () => {
+            console.log("peer connection is ready to receive data channel messages");
+        };
+
+        dataChannel.onmessage = (event) => {
+            console.log("message came from data channel");
+            const message = JSON.parse(event.data);
+            ui.appendMessage(message);
+        };
+    };
+
+    peerConection.onicecandidate = (event) => {
+        console.log("geeting ice candidates from stun server");
+        if (event.candidate) {
+            // send our ice candidates to other peer
+            wss.sendDataUsingWebRTCSignaling({
+                connectedUserSocketId: connectedUserDetails.socketId,
+                type: constants.webRTCSignaling.ICE_CANDIDATE,
+                candidate: event.candidate,
+            });
+        }
+    };
+
+    peerConection.onconnectionstatechange = (event) => {
+        if (peerConection.connectionState === "connected") {
+            console.log("succesfully connected with other peer");
+        }
+    };
+
+    // receiving tracks
+    const remoteStream = new MediaStream();
+    store.setRemoteStream(remoteStream);
+    ui.updateRemoteVideo(remoteStream);
+
+    peerConection.ontrack = (event) => {
+        remoteStream.addTrack(event.track);
+    };
+
+    // add our stream to peer connection
+
+    if (
+        connectedUserDetails.callType === constants.callType.VIDEO_PERSONAL_CODE
+    ) {
+        const localStream = store.getState().localStream;
+
+        for (const track of localStream.getTracks()) {
+            peerConection.addTrack(track, localStream);
+        }
+    }
 };
 
 export const sendPreOffer = (callType, calleePersonalCode) => {
